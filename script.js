312057f35c6abd515021e149fe86d11851e59686
hamzi-masnour000 <66399614+hamzi-masnour000@users.noreply.github.com>
2020-06-04 06:18:23 +0300

    added new files

---
+++ script.js
@@ -0,0 +1,17 @@
+const options={
+    bottom: '64px', // default: '32px'
+right: 'unset', // default: '32px'
+left: '10px', // default: 'unset'
+time: '0.5s', // default: '0.3s'
+mixColor: '#fff', // default: '#fff'
+backgroundColor: '#e6e5e2',  // default: '#fff'
+buttonColorDark: '#100f2c',  // default: '#100f2c'
+buttonColorLight: '#fff', // default: '#fff'
+saveInCookies: false, // default: true,
+label: '🌓', // default: ''
+autoMatchOsTheme: true
+}
+const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+  //////////////////////////////////////////
